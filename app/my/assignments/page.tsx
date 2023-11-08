91d59b8c5ef0f066ec0f93501400313586a5aeaa
Enes Buğra Gürbüz <enesbugragurbuz1@gmail.com>
2023-11-08 22:02:28 +0300

    update

--- app/my/assignments/page.tsx
+++ app/my/assignments/page.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const AssignmentsPage: React.FC = () => {
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center py-10">
+    <div className="body min-h-screen flex flex-col items-center justify-center py-10">
       
-      <div className="bg-white w-96 p-8 rounded-lg shadow-md">
-
-        <header className="mb-6">
+      <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-6xl bg-white p-8 shadow-lg rounded-lg h-full">
+      <img
+          src="https://bc3-production-assets-cdn.basecamp-static.com/5321781/people/BAhpBIISoQI=--54400445fe389a1609c1981816b42fd6f04890d5/avatar/in/260/3a3ea00cfc35332cedf6e5e9a32e94da.avif?v=1"
+          alt=""
+          className="mx-auto h-20 w-auto rounded-full"
+        />
+        <header className="mb-6 text-center mt-2">
           <h1 className="text-2xl font-semibold">Here are your assignments</h1>
         </header>
         
@@ -17,7 +21,7 @@ const AssignmentsPage: React.FC = () => {
           </label>
         </div>
         
-        <nav className="mb-6">
+        <nav className="mb-6 text-center">
           <button className="text-blue-600 font-semibold mr-8 pb-1 border-b-2 border-blue-600">My assignments</button>
           <button className="text-gray-400 font-semibold pb-1 border-b-2 border-transparent hover:border-gray-400 hover:text-gray-500">Stuff I've assigned</button>
         </nav>
